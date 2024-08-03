6f75b775f85da15b591029cdab5815c45f88c132
testetr <testetr@gmail.com>
2024-08-03 22:56:51 +0200

    Initializer

---
+++ RestoBook/src/test/java/pl/coderslab/RestoBook/RestoBookApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.RestoBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestoBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
